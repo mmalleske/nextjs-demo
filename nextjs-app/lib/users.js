5a865e1cab454adf1cdecf82bed6c78ca62afc08
Michael Malleske <mmalleske@gmail.com>
2023-03-30 00:24:37 -0500

    popups on map

--- nextjs-app/lib/users.js
+++ nextjs-app/lib/users.js
@@ -4,42 +4,60 @@ export const users = [
     name: "Ryu",
     country: "Japan",
     profilePic: "/images/Ryu.png",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    latitude: 36.2048,
+    longitude: 138.2529,
+    flagPic: "/images/japan-flag.png"
   },
   {
     id: "ken",
     name: "Ken",
     country: "United States",
     profilePic: "/images/Ken.png",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    latitude: 37.0902,
+    longitude: -95.7129,
+    flagPic: "/images/us-flag.webp"
   },
   {
     id: "blanka",
     name: "Blanka",
     country: "Brazil",
     profilePic: "/images/Blanka.png",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    latitude: -14.235,
+    longitude: -51.9253,
+    flagPic: "/images/brazil-flag.png"
   },
   {
     id: "chun-li",
     name: "Chun Li",
     country: "China",
     profilePic: "/images/Chun-Li.png",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    latitude: 35.8617,
+    longitude: 104.1954,
+    flagPic: "/images/china-flag.webp"
   },
   {
     id: "zangief",
     name: "Zangief",
-    country: "Soviet Union",
+    country: "Russia",
     profilePic: "/images/Zangief.png",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    latitude: 61.5240,
+    longitude: 105.3188,
+    flagPic: "/images/soviet-union-flag.webp"
   },
   {
     id: "dhalsim",
     name: "Dhalsim",
     country: "India",
     profilePic: "/images/dhalsim.png",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    latitude: 20.5937,
+    longitude: 78.9629,
+    flagPic: "/images/india-flag.png"
   },
 ];
 
--- nextjs-app/pages/map.js
+++ nextjs-app/pages/map.js
@@ -1,9 +1,10 @@
 import Layout from "../components/layout/layout";
 import { users } from "../lib/users";
-import { Map as MapGl, Layer, Feature, Marker } from "react-map-gl";
+import { Map as MapGl, Popup, Marker } from "react-map-gl";
 import { Divider, List, Typography } from "antd";
 import style from "./map.module.scss";
 import "mapbox-gl/dist/mapbox-gl.css";
+import Link from "next/link";
 
 export default function Map() {
   return (
@@ -26,9 +27,9 @@ export default function Map() {
       <div className={style.mapPage}>
         <MapGl
           initialViewState={{
-            latitude: -14.235,
-            longitude: -51.9253,
-            zoom: 3.5,
+            latitude: 0,
+            longitude: 0,
+            zoom: 1.5,
           }}
           style={{
             height: "100vh",
@@ -37,17 +38,34 @@ export default function Map() {
           mapStyle="mapbox://styles/mapbox/streets-v9"
           mapboxAccessToken="pk.eyJ1IjoibW1hbGxlc2tlIiwiYSI6ImNqNHA0ZGYwejAyOTEzNHA1Y3k3cXdxZGMifQ.FT4JkTtdKiqJ6xEG3fH0hw"
         >
-          {/* <Feature latitude={14.235} longitude={51.9253}></Feature> */}
-          <Marker
-            latitude={-14.235}
-            longitude={-51.9253}
-            anchor="bottom"
-            color="#0b6cb0"
-          >
-            <div className={style.marker}>
-              <img src="/images/brazil-flag.png" />
-            </div>
-          </Marker>
+          {users.map((user, index) => (
+            <>
+              <Popup
+                className={style.popup}
+                latitude={user.latitude}
+                longitude={user.longitude}
+                offset={{
+                  "bottom-left": [12, -38],
+                  bottom: [0, -38],
+                  "bottom-right": [-12, -38],
+                }}
+              >
+                <Link href={`/users/${user.id}`}>
+                  <img src={user.profilePic} />
+                </Link>
+              </Popup>
+              <Marker
+                latitude={user.latitude}
+                longitude={user.longitude}
+                anchor="bottom"
+                color="#0b6cb0"
+              >
+                <div className={style.marker}>
+                  <img src={user.flagPic} />
+                </div>
+              </Marker>
+            </>
+          ))}
         </MapGl>
       </div>
     </Layout>
